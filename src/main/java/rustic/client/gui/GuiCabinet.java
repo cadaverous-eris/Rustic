6059998f6358c53648de610e7c2031324f086f60
Graham Hughes <graham@sigwinch.org>
2017-12-01 00:46:17 -0600

    Add support for Quark's search bars, too.

--- src/main/java/rustic/client/gui/GuiCabinet.java
+++ src/main/java/rustic/client/gui/GuiCabinet.java
@@ -1,13 +1,18 @@
 package rustic.client.gui;
 
+import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.Optional;
 import rustic.common.tileentity.ContainerCabinet;
 import rustic.common.tileentity.TileEntityCabinet;
+import rustic.compat.Compat;
 import rustic.core.Rustic;
+import vazkii.quark.api.IItemSearchBar;
 
-public class GuiCabinet extends GuiContainer {
+@Optional.Interface(modid = Compat.QUARK, iface = "vazkii.quark.api.IItemSearchBar", striprefs = true)
+public class GuiCabinet extends GuiContainer implements IItemSearchBar {
 
 	public static final int WIDTH = 176;
     public static final int HEIGHT = 166;
@@ -45,4 +50,8 @@ public class GuiCabinet extends GuiContainer {
         this.fontRenderer.drawString(this.playerInv.getDisplayName().getUnformattedText(), 8, this.ySize - 96 + 2, 4210752);
     }
 
+	@Override
+	public void onSearchBarAdded(GuiTextField guiTextField) {
+		guiTextField.y++;
+	}
 }
