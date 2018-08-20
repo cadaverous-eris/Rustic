29040682550f7dc4184e6ea49354236356a6153d
Stuart Lindstrom <stuartlindstrom@gmail.com>
2018-08-20 04:31:49 -0400

    Fixed lattice item model

--- src/main/java/rustic/common/blocks/BlockLattice.java
+++ src/main/java/rustic/common/blocks/BlockLattice.java
@@ -13,10 +13,12 @@ import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
@@ -28,6 +30,7 @@ import net.minecraft.world.ColorizerFoliage;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeColorHelper;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.property.ExtendedBlockState;
 import net.minecraftforge.common.property.IExtendedBlockState;
 import net.minecraftforge.common.property.IUnlistedProperty;
@@ -207,7 +210,7 @@ public class BlockLattice extends BlockBase implements IColoredBlock {
 	
 	@Override
 	public void initModel() {
-		super.initModel();
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName().toString() + "_item","inventory"));
 		ClientProxy.addColoredBlock(this);
 	}
 	
