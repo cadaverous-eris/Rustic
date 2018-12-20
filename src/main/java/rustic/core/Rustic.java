f58d68a395aeb806d4374697671fd434b649e909
Stuart Lindstrom <stuartlindstrom@gmail.com>
2018-12-19 19:32:06 -0500

    Updated version number

--- src/main/java/rustic/core/Rustic.java
+++ src/main/java/rustic/core/Rustic.java
@@ -41,7 +41,7 @@ import rustic.compat.crafttweaker.CraftTweakerHelper;
 public class Rustic {
 	public static final String MODID = "rustic";
 	public static final String NAME = "Rustic";
-	public static final String VERSION = "1.0.13";
+	public static final String VERSION = "1.0.14";
 	public static final String DEPENDENCIES = "after:dynamictrees@[1.12.2-0.9.1a,);after:dynamictreesbop;before:dynamictreestc";
 
 	@SidedProxy(clientSide = "rustic.core.ClientProxy", serverSide = "rustic.core.CommonProxy")
